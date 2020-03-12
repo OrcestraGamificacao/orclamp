505e233fdb18a661a81f16bda9acc8ed2a9ee5e5
Fellipe Araujo <araujo.fellipe@hotmail.com>
2020-03-12 18:14:49 -0300

    #10 - Created Login Page.
    Co-authored-by: João Guedes <isudjoao@gmail.com>

---
+++ mobile/src/pages/Main/LoginPage/styles.js
@@ -0,0 +1,55 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  container_logo: {
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: '10%',
+  },
+  container_input_data: {
+    paddingTop: '5%',
+  },
+  signin: {
+    fontSize: 32,
+    alignSelf: 'flex-start',
+    paddingLeft: '5%'
+  },
+  input_username_title: {
+    paddingTop: '5%',
+    paddingLeft: '5%',
+  },
+  input_username: {
+    alignSelf: 'center',
+    paddingTop: '2%',
+    width: '95%',
+  },
+  input_password_title: {
+    paddingTop: '7%',
+    paddingLeft: '5%',
+  },
+  input_password: {
+    alignSelf: 'center',
+    paddingTop: '2%',
+    width: '95%'
+  },
+  container_button: {
+    paddingTop: '10%',
+  },
+  button_login: {
+    alignSelf: 'center',
+    height: 56,
+    width: '80%',
+    backgroundColor: '#7AC14E',
+  },
+  container_text: {
+    paddingTop: '5%',
+  },
+  last_message: {
+    alignSelf: 'center',
+  },
+});
+
+export default styles;
