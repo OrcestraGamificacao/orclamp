b81e8d14c676235737598cc25f8bbe3a76923127
Fellipe Araujo <araujo.fellipe@hotmail.com>
2020-04-03 13:10:52 -0300

    #4 - Developed NewProject page.

--- frontend/src/pages/NewProject/index.js
+++ frontend/src/pages/NewProject/index.js
@@ -1,7 +1,42 @@
 import React from 'react';
 
+import './styles.css';
+
+import logoImg from '../../assets/logo.png';
+
 export default function NewProject() {
   return (
-    <h1>Criação de Projetos</h1>
+    <div className="new-project-content">
+      <div className="content">
+        <section>
+          <img src={logoImg} alt="Orclamp" />
+        </section>
+
+        <form>
+          <h1>Criar Projeto</h1>
+          <input
+            className="project"
+            placeholder="Nome do Projeto"
+            onChange={() => { }}
+          />
+
+          <input
+            className="technology"
+            placeholder="Tecnologias"
+            onChange={() => { }}
+          />
+
+          <textarea
+            placeholder="Descrição"
+            onChange={() => { }}
+          />
+
+          <button className="button" type="submit">
+            Próximo
+          </button>
+
+        </form>
+      </div>
+    </div>
   );
 }
