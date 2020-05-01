00b73bc71b2f8a3f0536dfb9b9ea8c928f73b550
Fellipe Araujo <araujo.fellipe@hotmail.com>
2020-05-01 20:14:52 -0300

    #32 - Added route to login.

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import NewProject from './pages/NewProject';
 import ConfirmProject from './pages/ConfirmProject';
 import Register from './pages/Register';
+import Login from './pages/Login';
 
 export default function Routes() {
   return (
@@ -12,6 +13,7 @@ export default function Routes() {
         <Route path="/project/new" exact component={NewProject} />
         <Route path="/project/new/confirm" component={ConfirmProject} />
         <Route path="/register" component={Register} />
+        <Route path="/login" component={Login} />
       </Switch>
     </BrowserRouter>
   );
